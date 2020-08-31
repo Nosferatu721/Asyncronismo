24fd5367902639d6734b68de398c2ed16b9a2f8e
Nosferatu721 <elkintorres721@gmail.com>
2020-08-31 11:29:43 -0500

    Mini Api

---
+++ src/callback/challenge.js
@@ -0,0 +1,39 @@
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const API = "https://rickandmortyapi.com/api/character/";
+
+// Estados
+// 0 - Inicializado
+// 1 - Cargando (Proceso de Hacer el Llamado)
+// 2 - Cargado (Datos Recividos)
+// 3 - Procesado
+// 4 - Completado
+
+const fetchData = (url_api, callback) => {
+  let xhttp = new XMLHttpRequest();
+  xhttp.open("GET", url_api, true);
+  xhttp.onreadystatechange = (event) => {
+    if (xhttp.readyState === 4) {
+      if (xhttp.status === 200) {
+        callback(null, JSON.parse(xhttp.responseText));
+      } else {
+        const error = new Error("Error (F): " + url_api);
+        return callback(error, null);
+      }
+    }
+  };
+  xhttp.send();
+};
+
+fetchData(API, (error1, data1) => {
+  if (error1) return console.error(error1);
+  fetchData(API + data1.results[0].id, (error2, data2) => {
+    console.log(API + data1.results[0].id);
+    if (error2) return console.error(error2);
+    fetchData(data2.origin.url, (error3, data3) => {
+      if (error3) return console.error(error3);
+      console.log(data1.info.count);
+      console.log(data2.name);
+      console.log(data3.dimension);
+    });
+  });
+});
